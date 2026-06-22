a6813ae52e62b0fe1dee1d9da6318e71f926a42d
srangana <srangana@opentext.com>
2026-06-22 11:57:51 +0530

    Initial commit of hitl project files

---
+++ src/main/java/com/bookbot/hitl/HitlApplication.java
@@ -0,0 +1,13 @@
+package com.bookbot.hitl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HitlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HitlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bookbot/hitl/HitlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookbot.hitl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HitlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
